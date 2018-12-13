7672974121737450f76d7bdd17c8bcf6dac83bc8
Aize Igbinakenzua <43158853+ize202@users.noreply.github.com>
2018-12-13 22:05:30 +0100

    Flowing Story
    an interactive story app

---
+++ audio.swift
@@ -0,0 +1,35 @@
+//
+//  audio.swift
+//  FlowingStory
+//
+//  Created by Aize Igbinakenzua on 2018-12-12.
+//  Copyright © 2018 Aize Igbinakenzua. All rights reserved.
+//
+
+import Foundation
+import AudioToolbox
+
+extension Story {
+    var soundEffectName: String {
+        switch self {
+        case .droid, .home: return "HappyEnding"
+        case .monster: return "Ominous"
+        default: return "PageTurn"
+        }
+    }
+    
+    var soundEffectURL: URL {
+        let path = Bundle.main.path(forResource: soundEffectName, ofType: "wav")!
+        return URL(fileURLWithPath: path)
+    }
+}
+
+class SoundEffectsPlayer {
+    var sound: SystemSoundID = 0
+    
+    func playSound(for story: Story) {
+        let soundURL = story.soundEffectURL as CFURL
+        AudioServicesCreateSystemSoundID(soundURL, &sound)
+        AudioServicesPlaySystemSound(sound)
+    }
+}
